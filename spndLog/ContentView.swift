bbd887fd5dcff11230f69cf94cff7331189d3506
isemae <foxlis97@naver.com>
2023-11-23 02:54:25 +0900

    feat: showing/hiding keypad

--- spndLog/ContentView.swift
+++ spndLog/ContentView.swift
@@ -12,31 +12,55 @@ struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
 	@State private var string = "0"
-	
+	@State private var isShowingKeypad = false
 	let minDistance = 10.0
-	
 	
 	var body: some View {
 		let bounds = UIScreen.main.bounds
 		var width = bounds.size.width
 		var height = bounds.size.height
+		
 		let groupedItems = groupItemsByDate(items: items)
 		NavigationSplitView {
-			List {
-				ForEach(groupedItems.reversed(), id: \.0) { date, itemsInDate in
-					Section(header: Text("\(date, format: Date.FormatStyle(date: .long, time: .none))")) {
-						ForEach(itemsInDate, id: \.id) { item in
-							HStack {
-								Text(" \(item.balance) ")
-								Spacer()
-								Text(" \(item.timestamp, format: Date.FormatStyle(date: .none, time: .shortened))")
-									.foregroundColor(.gray)
-							}.padding(5)
+			var dayTotal: Int = 0
+			ScrollView {
+				ForEach(groupedItems, id: \.0) { date, itemsInDate in
+					HStack(alignment: .top) {
+						VStack {
+							Text("\(date, format: Date.FormatStyle(date: .numeric, time: .none))")
+								.font(.title2)
+								.foregroundColor(.blue)
 						}
+						VStack(alignment: .trailing) {
+							VStack {
+								ForEach(itemsInDate, id: \.id) { item in
+									HStack {
+										Text("₩\(item.balance)")
+											.onAppear {
+												if let balance = Int(item.balance) {
+													dayTotal += balance
+												}
+											}
+											.font(.title)
+										Spacer()
+										Text("\(item.timestamp, format: Date.FormatStyle(date: .none, time: .shortened))")
+											.foregroundColor(.gray)
+											.font(.title2)
+									}
+									.transition(.move(edge: .bottom).combined(with: .opacity))
+								}
+								.onDelete(perform: { indexSet in
+									deleteItems(offsets: indexSet)
+									dayTotal = itemsInDate.reduce(0) { $0 + Int($1.balance)! }
+								})
+							}
+							Divider()
+							
+						}
+						.padding(5)
 					}
 				}
-				.onDelete(perform: deleteItems)
-			}
+			}.padding()
 			.toolbar {
 				ToolbarItem(placement: .navigationBarTrailing) {
 					EditButton()
@@ -53,15 +77,21 @@ struct ContentView: View {
 				}
 			}
 		} detail: {
-			Text("Select an item")
+			
 		}
 		VStack {
 			HStack {
+				Text("¥")
 				Spacer()
 				Text(string)
 			}
 			.contentShape(Rectangle())
 			.padding([.leading, .trailing])
+			.onTapGesture(perform: {
+				withAnimation(.spring(response: 0.2, dampingFraction: 1.0)) {
+					isShowingKeypad.toggle()
+				}
+			})
 			.gesture(
 				DragGesture()
 					.onChanged { orientation in
@@ -72,48 +102,58 @@ struct ContentView: View {
 						
 						if trans.height < minDistance {
 							if string != "0" {
-								UISelectionFeedbackGenerator().selectionChanged()
 								addItem()
+								UISelectionFeedbackGenerator().selectionChanged()
 							}
 						}
-						
-						if ((orientation.location.y - orientation.startLocation.y) > 0) {
-							UISelectionFeedbackGenerator().selectionChanged()
+				
+					}
+					.onEnded { orientation in
+						let trans = orientation.translation
+						if (trans.height > minDistance) {
 							deleteFirst()
+							UISelectionFeedbackGenerator().selectionChanged()
 						}
 					}
-					.onEnded { orientation in }
 			)
 			Divider()
 			Keypad(string: $string)
+				.frame(height: isShowingKeypad ? height / 3.5 : 0)
+				.opacity(isShowingKeypad ? 1 : 0)
+				.offset(y: isShowingKeypad ? 0 : height)
+				.transition(.move(edge: .bottom))
 		}
 		.font(.largeTitle)
 		.padding()
 	}
 	
-    private func addItem() {
+	private func addItem() {
 			let newItem = Item(timestamp: Date(), balance: string)
+			modelContext.insert(newItem)
 			string = "0"
-            modelContext.insert(newItem)
-    }
-
+	}
+	
     private func deleteItems(offsets: IndexSet) {
-        withAnimation {
+        
             for index in offsets {
                 modelContext.delete(items[index])
             }
-        }
+        
     }
 	
 	private func deleteFirst() {
-		let firstItem = items.firstIndex
-//		modelContext.delete(firstItem)
+			if let recentItem = items.last {
+				withAnimation {
+				modelContext.delete(recentItem)
+			}
+		}
 	}
 	
 	// for dev only
 	private func deleteAll() {
 		do {
-			try modelContext.delete(model: Item.self)
+			try? modelContext.fetch(FetchDescriptor<Item>()).forEach { modelContext.delete($0)}
+			try? modelContext.save()
 		} catch {
 			fatalError(error.localizedDescription)
 		}
@@ -125,7 +165,12 @@ struct ContentView: View {
 
 		let sortedGroups = groupedDictionary.sorted { $0.key > $1.key }
 
-		return sortedGroups
+		
+		let sortedItemsInGroups = sortedGroups.map { (date, items) in
+			(date, items.sorted(by: { $0.timestamp > $1.timestamp }))
+		}
+
+		return sortedItemsInGroups
 	}
 }
 
