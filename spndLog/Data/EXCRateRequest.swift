53c13df2ce5162570cf77630b54ba4a0f73e1611
isemae <foxlis97@naver.com>
2023-11-23 06:37:33 +0900

    feat: API data fetcher

---
+++ spndLog/Data/EXCRateRequest.swift
@@ -0,0 +1,94 @@
+//
+//  EXCRateResponse.swift
+//  spndLog
+//
+//  Created by Jiwoon Lee on 11/23/23.
+//
+
+import Foundation
+
+struct Response: Decodable {
+	var result: Int
+	var cur_unit: String
+	var ttb: String
+	var tts: String
+	var deal_bas_r: String
+	var bkpr: String
+	var yy_efee_r: String
+	var ten_dd_efee_r: String
+	var kftc_bkpr: String
+	var kftc_deal_bas_r: String
+	var cur_nm: String
+}
+
+enum HTTPMethod: String {
+	case get = "GET"
+	case post = "POST"
+}
+
+struct ExchangeURL {
+	var urlComponents = URLComponents()
+
+	var url: URL? {
+		return urlComponents.url
+	}
+
+	init(authKey: String, theDateBefore: String) {
+		urlComponents.scheme = "https"
+		urlComponents.host = "www.koreaexim.go.kr"
+		urlComponents.path = "/site/program/financial/exchangeJSON"
+		urlComponents.queryItems = [
+			URLQueryItem(name: "authkey", value: authKey),
+			URLQueryItem(name: "searchdate", value: theDateBefore),
+			URLQueryItem(name: "data", value: "AP01"),
+		]
+	}
+}
+
+
+func request(url: String, method: HTTPMethod, param: [String: Any]? = nil, completionHandler: @escaping (Result<Int, Error>) -> Void) {
+	guard let url = URL(string: url) else {
+		print("Error: Cannot create URL")
+		completionHandler(.failure(NSError(domain: "URLCreationError", code: 0, userInfo: nil)))
+		return
+	}
+
+	var request = URLRequest(url: url)
+	request.httpMethod = method.rawValue
+
+	if let param = param {
+		let sendData = try! JSONSerialization.data(withJSONObject: param, options: [])
+		request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+		request.httpBody = sendData
+	}
+	
+	let session = URLSession.shared
+	
+	session.dataTask(with: request) { data, response, error in
+		guard error == nil else {
+			print("Error: \(error!)")
+			completionHandler(.failure(error!))
+			return
+		}
+
+		guard let data = data else {
+			print("Error: Could not receive data")
+			completionHandler(.failure(NSError(domain: "NoDataError", code: 0, userInfo: nil)))
+			return
+		}
+
+		guard let response = response as? HTTPURLResponse, (200 ..< 300) ~= response.statusCode else {
+			print("Error: HTTP request failed")
+			completionHandler(.failure(NSError(domain: "HTTPError", code: 0, userInfo: nil)))
+			return
+		}
+
+		do {
+			let output = try JSONDecoder().decode([Response].self, from: data)
+			completionHandler(.success(output.first!.result))
+		} catch {
+			print("Error: JSON Data Parsing failed")
+			completionHandler(.failure(error))
+		}
+	}.resume()
+}
