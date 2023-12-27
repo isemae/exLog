37972427665a87612529022cbb2da0b56191d237
isemae <foxlis97@naver.com>
2023-12-27 17:02:00 +0900

    feat : ImagePickerView구현 fix : OverlayKeypad가 리스트 하단을 가리는 문제

--- exLog/extensions.swift
+++ exLog/extensions.swift
@@ -20,6 +20,16 @@ extension Color {
 	}
 }
 
+extension Text {
+	func getContrastText(backgroundColor: Color) -> some View {
+		var r,g,b,a: CGFloat
+		(r,g,b,a) = (0,0,0,0)
+		UIColor(backgroundColor).getRed(&r, green: &g, blue: &b, alpha: &a)
+		let luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b
+		return luminance < 0.6 ? self.foregroundColor(.white) : self.foregroundColor(.black)
+	}
+}
+
 extension View {
 	func safeAreaOverlay(alignment: Alignment, edges: Edge.Set) -> some View {
 		self.overlay(alignment: alignment) {
