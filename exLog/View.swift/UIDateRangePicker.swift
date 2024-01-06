c5a5480b27a826707f2dd21d79dc91bfd6c55534
isemae <foxlis97@naver.com>
2024-01-07 00:09:40 +0900

    Refac: UIDateRangePicker 파일 분리

---
+++ exLog/View.swift/UIDateRangePicker.swift
@@ -0,0 +1,47 @@
+//
+//  UIDateRangePicker.swift
+//  exLog
+//
+//  Created by Jiwoon Lee on 1/6/24.
+//
+
+import SwiftUI
+import UIKit
+
+struct DateRangePicker: UIViewRepresentable {
+	@Binding var selectedDates: [Date]
+	
+	func makeUIView(context: Context) -> some UIDatePicker {
+		let datePicker = UIDatePicker()
+		datePicker.datePickerMode = .date
+		datePicker.addTarget(context.coordinator, action: #selector(Coordinator.dateChanged(_:)), for: .valueChanged)
+		return datePicker
+	}
+	func updateUIView(_ uiView: UIViewType, context: Context) {
+		
+	}
+	
+	func makeCoordinator() -> Coordinator {
+		Coordinator(self)
+	}
+	
+	class Coordinator: NSObject {
+		var parent: DateRangePicker
+		
+		init(_ parent: DateRangePicker) {
+			self.parent = parent
+		}
+		
+		@objc func dateChanged(_ sender: UIDatePicker) {
+			let selectedDate = sender.date
+			
+			parent.selectedDates.removeAll(where: { $0 == selectedDate })
+			parent.selectedDates.append(selectedDate)
+			
+			if parent.selectedDates.count > 2 {
+				parent.selectedDates.removeAll()
+				parent.selectedDates.append(selectedDate)
+			}
+		}
+	}
+}
