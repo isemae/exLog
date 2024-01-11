1aa363532f24f0a7e8f93845a11373a50e4d0642
isemae <foxlis97@naver.com>
2024-01-12 05:14:45 +0900

    Feat: 그리드 셀 개별삭제 구현

--- exLog/View.swift/LocationGridView.swift
+++ exLog/View.swift/LocationGridView.swift
@@ -6,9 +6,12 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct LocationGridView: View {
-	var locations: [Location]
+	@Environment(\.modelContext) private var modelContext
+	@Query var locations: [Location]
+	@State var showImagePicker = false
 	var items: [Item]
 	var tapAction: () -> Void
 
@@ -16,25 +19,44 @@ struct LocationGridView: View {
 		LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 10) {
 			ForEach(locations.sorted(by: { $0.startDate ?? Date()  > $1.startDate ?? Date() }), id: \.self) { location in
 				ZStack {
-					ImagePickerView()
+					ImagePickerView(locations: locations, showImagePicker: $showImagePicker)
 					VStack {
-						NavigationLink(location.name, destination: ItemListView(items: items.filter { item in
-							if let startDate = location.startDate, let endDate = location.endDate {
-								return startDate <= item.date && item.date <= endDate
-							}
-							return false
-						}, onTap: tapAction ))
+						NavigationLink(location.name, destination: destinationItemListView(location: location))
 						.font(.title)
 						.foregroundColor(.primary)
 						.bold()
 						Text("\(formattedDate(date: location.startDate ?? Date())) ~ \(formattedDate(date: location.endDate ?? Date()))")
 					}
 				}
+				.contextMenu(ContextMenu(menuItems: {
+					Button {
+						showImagePicker.toggle()
+					} label: {
+						Label("배경이미지 설정", systemImage: "photo.badge.plus")
+					}
+					Button(role: .destructive) {
+						if let index = locations.firstIndex(where: { $0.id == location.id }) {
+							modelContext.delete(location)
+							try? modelContext.save()
+						}
+					} label : {
+						Label("여행계획 삭제", systemImage: "trash")
+					}
+				}))
 			}
 		}
 		.padding()
 		.navigationTitle("main")
     }
+
+	func destinationItemListView(location: Location) -> some View {
+		ItemListView(items: items.filter { item in
+			if let startDate = location.startDate, let endDate = location.endDate {
+				return startDate <= item.date && item.date <= endDate
+			}
+			return false
+		}, onTap: tapAction )
+	}
 }
 //
 // #Preview {
--- exLog/View.swift/Pickers/ImagePickerView.swift
+++ exLog/View.swift/Pickers/ImagePickerView.swift
@@ -8,7 +8,8 @@
 import SwiftUI
 
 struct ImagePickerView: View {
-	@State var showImagePicker = false
+	@State var locations : [Location]
+	@Binding var showImagePicker: Bool
 	@State var selectedUIImage: UIImage?
 	@State var image: Image?
 
@@ -31,9 +32,7 @@ struct ImagePickerView: View {
 		}
 
 		.sheet(isPresented: $showImagePicker, onDismiss: { loadImage() }) { ImagePicker(image: $selectedUIImage) }
-		.onLongPressGesture(perform: {
-			showImagePicker.toggle()
-		})
+
 	}
 
 	func loadImage() {
@@ -41,7 +40,7 @@ struct ImagePickerView: View {
 		image = Image(uiImage: selectedImage)
 	}
 }
-
-#Preview {
-	ImagePickerView()
-}
+//
+// #Preview {
+//	ImagePickerView()
+// }
